963aa1165d2deb5bbd8ceefe94b6c1c4db9ae58f
AndresVercesi <andresvercesi@gmail.com>
2024-03-01 14:19:53 -0500

    Change video position

--- pages/st_obj_counter.py
+++ pages/st_obj_counter.py
@@ -9,63 +9,50 @@ from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 import os
 
+det_box = []
 
 
 path_base = os.path.abspath(os.path.dirname(__file__))
 uploads_path = os.path.join(path_base, 'uploads')
 model_path = 'yolov8n.pt'
 
-det_box =[]
+def load_model(model_path):
+    """
+    Load Yolo Object detection model from specified model_path
+    
+    Parameters:
+        model_path (str): The path to the Yolo model file
 
-if not os.path.exists(uploads_path):
-    os.makedirs(uploads_path)
-
-try:
+    Returns:
+        A Yolo object detection model
+    """
     model = YOLO(model_path)
-except Exception as ex:
-    st.error(
-        f"Unable to load model. Check the specified path: {model_path}")
-    st.error(ex)
+    return model
 
+def draw_det_box(video_file):
+    """
+    Draw a rectangle in first frame of video file to select
+    detection area
 
-st.write("Objects count in video using YOLO") #Web page title
+    Parameters:
+        video_file (str): The path of video file
 
-video_file = st.file_uploader('Video File')
-
-option = st.selectbox(
-    'Select count type',
-    ('Bounding Box', 'Line'))
-if option == 'Bounding Box':
-    detection_type = 'polygon'
-    detection_range = 4
-if option == 'Line':
-    detection_type = 'line'
-    detection_range = 2
-
-
-if video_file is not None:
-   
-    vid = os.path.join(uploads_path, video_file.name )
-    with open(vid, mode='wb') as f:
-        f.write(video_file.read()) # save video to disk
-
-    vidcap = cv2.VideoCapture(vid) # load video from disk
+    Returns:
+        List[int] with 4 points of detection box 
+    """
+    det_box = []
+    vidcap = cv2.VideoCapture(video_file) # load video from disk
     w, h = (int(vidcap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT))
-    w = int(w/2)
-    h = int(h/2)
+    w = int(w/2.8)
+    h = int(h/2.8)
     success, frame = vidcap.read()
-    frame = cv2.resize(frame, (w, h))
-    bg_image = cv2.imwrite('first_frame.jpg', frame)
-    #pil_img = Image.fromarray(destRGB) # convert opencv frame (with type()==numpy) into PIL Image
-    #st.image(pil_img, channels='BGR')
-    #stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-    #stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-    #print(stroke_color)
+    if success:
+        frame = cv2.resize(frame, (w, h))
+        bg_image = cv2.imwrite('first_frame.jpg', frame)
+    
     bg_color = "#eee"
-    #bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
     bg_image = 'first_frame.jpg'
-    #realtime_update = st.sidebar.checkbox("Update in realtime", True)
-    # Create a canvas component
+    
     canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=3,
@@ -73,28 +60,53 @@ if video_file is not None:
     background_color=bg_color,
     background_image=Image.open(bg_image) if bg_image else None,
     update_streamlit=True,
-    height=h,
+    height= h,
     width = w,
-    drawing_mode=detection_type,
+    drawing_mode='polygon',
     display_toolbar= True,
-    key="full_app",
-)
-    # Do something interesting with the image data and paths
-    #if canvas_result.image_data is not None:
-    #    st.image(canvas_result.image_data)
+    key="canvas",
+    )
+   
     if canvas_result.image_data is not None:
         if len(canvas_result.json_data['objects'])==1:
-            for i in range(detection_range):
+            for i in range(4):
                 p = []
                 for j in range(2):
                     p.append(canvas_result.json_data['objects'][0]['path'][i][j+1])
                 det_box.append(p)
-            print(len(det_box))
-            
+    os.remove(bg_image)
+    return (det_box, w, h)
 
+#Main page title
+st.title("Objects count in video using YOLO")
+
+if not os.path.exists(uploads_path):
+    os.makedirs(uploads_path)
+
+try:
+    model = load_model(model_path)
+except Exception as ex:
+    st.error(
+        f"Unable to load model. Check the specified path: {model_path}")
+    st.error(ex)
+
+
+
+video_file = st.file_uploader('Video File')
+
+
+st_frame = st.empty()
 process_button = st.button('Proccess video')
 
-if process_button==True and len(det_box)==4:
+if video_file is not None:
+    vid = os.path.join(uploads_path, video_file.name )
+    with open(vid, mode='wb') as f:
+        f.write(video_file.read()) # save video to disk
+    det_box, w, h = draw_det_box(vid)
+
+
+
+if process_button==True:
     # Init Object Counter
     counter = object_counter.ObjectCounter()
     counter.set_args(view_img=True,
@@ -102,7 +114,7 @@ if process_button==True and len(det_box)==4:
                  classes_names=model.names,
                  draw_tracks=True)
     vid_cap = cv2.VideoCapture(vid)
-    st_frame = st.empty()
+    
     while (vid_cap.isOpened()):
         success, image = vid_cap.read()
         if success:
@@ -113,10 +125,10 @@ if process_button==True and len(det_box)==4:
             #result_tensor = res[0].boxes
             #res_plotted = res[0].plot()
             st_frame.image(image,
-                               caption='Detected Video',
-                               channels="BGR",
-                               use_column_width=True
-                               )
+                            caption='Detected Video',
+                            channels="BGR",
+                            use_column_width=True
+                            )
         else:
             vid_cap.release()
             break
